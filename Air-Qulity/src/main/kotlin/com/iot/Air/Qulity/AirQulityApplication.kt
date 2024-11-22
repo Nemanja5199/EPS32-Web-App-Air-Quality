0c4d6db226182e5c8ea1d6ee6de4da14d4e14986
Nemanja <bogojevicn5199@gmail.com>
2024-11-22 13:51:29 +0100

    First commit

---
+++ Air-Qulity/src/main/kotlin/com/iot/Air/Qulity/AirQulityApplication.kt
@@ -0,0 +1,11 @@
+package com.iot.Air.Qulity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirQulityApplication
+
+fun main(args: Array<String>) {
+	runApplication<AirQulityApplication>(*args)
+}
